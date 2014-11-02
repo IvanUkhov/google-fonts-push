09dc4891d936a5c0ff250bece1c3afce8540751c
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-02 20:25:01 +0100

    Switched from a configuration file to command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
+#![feature(macro_rules)]
+
 extern crate git;
-extern crate serialize;
 
-use config::Config;
+use std::os::args;
+use git::Error;
 
-mod config;
+macro_rules! ok(
+    ($result:expr) => (
+        match $result {
+            Ok(_) => {},
+            Err(e) => {
+                error(e);
+                return;
+            },
+        }
+    );
+)
 
 fn main() {
-    println!("Hello, world!");
+    let args = args();
+
+    if args.len() != 3 {
+        usage();
+        return;
+    }
+
+    let (command, path) = (args[1].clone(), Path::new(args[2].clone()));
+
+    match command.as_slice() {
+        "status" => ok!(status(&path)),
+        "push" => ok!(push(&path)),
+        _ => usage(),
+    }
+}
+
+fn error(e: Error) {
+    println!("{}", e);
+}
+
+fn usage() {
+    println!("Usage: {} (status|push) <path>", args()[0]);
+}
+
+fn status(_path: &Path) -> Result<(), Error> {
+    Ok(())
+}
+
+fn push(_path: &Path) -> Result<(), Error> {
+    Ok(())
 }
